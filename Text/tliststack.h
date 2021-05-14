31f0441312a7e71f003d267c01fc9fe169406fd5
MBarysheva <mari.barysheva.2001@mail.ru>
2021-05-14 19:38:07 +0300

    comm 14.05

---
+++ Text/tliststack.h
@@ -0,0 +1,134 @@
+#ifndef _tliststack_h
+#define _tliststack_h
+
+#include <iostream>
+
+//list link
+template <class T>
+struct TNode {
+	T val; //value
+	TNode<T>* pNext; //poiter to the next link
+};
+
+//stack class
+template <class T>
+class TStack {
+	TNode<T>* pFirst;
+public:
+
+	//constructor
+	TStack() {
+		pFirst = NULL;
+	}
+
+	//destructor
+	~TStack() {
+		TNode<T>* tmp = pFirst;
+		if (tmp == NULL)
+			delete tmp;
+		while (pFirst != NULL) {
+			pFirst = pFirst->pNext;
+			delete tmp;
+			tmp = pFirst;
+		}
+	}
+
+	//copy constructor
+	TStack(const TStack<T>& st) {
+		TNode<T>* tmp = st.pFirst;
+		if (tmp == NULL)
+			pFirst = tmp;
+		while (tmp != NULL) {
+			Push(tmp->val);
+			tmp = tmp->pNext;
+		}
+		delete tmp;
+	}
+
+	//assignment operator
+	TStack<T> operator=(const TStack<T>& st) {
+		if (this != &st) {
+			if (!IsEmpty())
+				Clear();
+			else {
+				TNode<T>* tmp = st.pFirst;
+				if (tmp == NULL)
+					pFirst = tmp;
+				while (tmp != NULL) {
+					Push(tmp->val);
+					tmp = tmp->pNext;
+				}
+				delete tmp;
+			}
+		}
+		return *this;
+	}
+
+	//check for emptyness
+	bool IsEmpty() const {
+		return pFirst == NULL;
+	}
+
+	//check for full
+	bool IsFull() const {
+		TNode<T>* tmp;
+		tmp = new TNode<T>;
+		if (tmp == NULL)
+			return 1;
+		else {
+			delete tmp;
+			return 0;
+		}
+	}
+
+	//insetr into stack
+	void Push(const T& a) {
+		if (IsFull())
+			throw - 1;
+		else {
+			TNode<T>* tmp;
+			tmp = new TNode<T>;
+			tmp->pNext = pFirst;
+			tmp->val = a;
+			pFirst = tmp;
+		}
+	}
+
+	//extract from stack
+	T Pop() {
+		if (IsEmpty())
+			throw - 1;
+		else {
+			T res = pFirst->val;
+			TNode<T>* tmp;
+			tmp = pFirst;
+			pFirst = pFirst->pNext;
+			delete tmp;
+			return res;
+		}
+	}
+
+	//see what on the top of stack
+	T Top() {
+		if (IsEmpty())
+			throw - 1;
+		else
+			return pFirst->val;
+	}
+
+	//clean stack
+	void Clear() {
+		TNode<T>* tmp = pFirst;
+		while (pFirst != NULL) {
+			pFirst = pFirst->pNext;
+			delete tmp;
+			tmp = pFirst;
+		}
+	}
+
+	bool GetSize() {
+		return size;
+	}
+};
+#endif 
+
---
+++ Text/ttext.h
@@ -0,0 +1,48 @@
+#ifndef _ttext_h_
+#define _ttext_h_
+
+#include "ttextlink.h"
+#include "tstack.h"
+#include <string>
+
+class TText
+{
+	TTextLink* pFirst;
+	TTextLink* pCurr;
+	TStack<TTextLink*> stack;
+
+public:
+
+	TText(); //constructor
+	void GoFirstLink(); //go to beginning of text
+	void GoDownLink(); //go to the down link
+	void GoNextLink(); //go to the next link
+	void GoPrevLink(); //go to previous link
+	void SetLine(std::string astr); //adding a line to text
+	std::string GetLine(); //taking a line from text
+
+	//Structural modification methods
+
+	void InsNextLine(std::string astr); //adding the next line
+	void InsNextSection(std::string astr); //adding the next sectoin
+	void InsDownLine(std::string astr); //adding the lower line
+	void InsDownSection(std::string astr); //adding the lower sectoin
+	void DelNext(); //delete  the next link
+	void DelDownLine(); // delete the down link
+
+	//Reaind and writing methods
+
+	void Read(std::string fn); //read file
+	TTextLink* ReadRec(std::ifstream& ifs); //readig lines from a file
+	void Write(std::string fn); //write to file
+	void WriteRec(std::ofstream& ofs, TTextLink* pWC); // recursive writing function
+	void ConsolePrint(); //write to console
+	void ConsolePrintRec(TTextLink* pWC); // recursive printing function to console
+
+	//Text navigetion methods
+
+	int Reset(); //set to first link
+	int IsEnd(); //end check
+	int GoNext(); // skip to the next link
+};
+#endif;
